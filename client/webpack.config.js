a7192bfc87796927c74a5ff57d12ae5a52459008
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-20 17:24:59 -0500

    added rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -44,7 +44,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        },
       ],
     },
   };
