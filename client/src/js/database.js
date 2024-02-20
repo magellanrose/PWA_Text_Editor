e960e461c5801d1e22f1f103dfcbbf06dc717d5a
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-02-20 17:20:51 -0500

    database.js and install files are done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const textEditDb = await openDB('jate', 1);
+    const txt = textEditDb.transaction('jate', 'readwrite');
+    const storeTxt = txt.objectStore('jate');
+    const request = storeTxt.put({
+      value: content,
+      id: 1
+    })
+    const output = await request;
+    console.log('Data added to DB', output)
+  } catch (error) {
+    console.error('putDb not implemented')
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const textEditDb = await openDB('jate', 1);
+    const txt = textEditDb.transaction('jate', 'readonly');
+    const storeTxt = txt.objectStore('jate');
+    const request = storeTxt.get(1);
+    const output = await request;
+    console.log('output.value', output)
+    return output?.value;
+  } catch (error) {
+    console.error('getDb not implemented')
+  }
+};
+
 
 initdb();
